dab8b455088e6d4f9e5c7f0f32f9ea5757938296
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2022-12-29 18:59:36 +0530

    Done with updation of slider value

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const lengthSlider = document.querySelector(".pass-length input");
+
+const updateSlider = () => {
+	document.querySelector(".details span").innerText = lengthSlider.value;
+}
+
+updateSlider();
+
+lengthSlider.addEventListener("input", updateSlider);
